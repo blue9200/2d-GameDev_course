782f6a5e61346c08e5592f741dcf4d28d58cf89e
blue9200 <Josantoyo@s.sfusd.edu>
2024-03-01 12:42:44 -0800

    Add a chest that spawns pickups

--- 02.getting-to-know-godot/levels/rooms/chests/chest.gd
+++ 02.getting-to-know-godot/levels/rooms/chests/chest.gd
@@ -11,8 +11,7 @@ const LOOT_SCENES := [
 
 func loot() -> void:
 	_animation_player.play("open")
-
-
+	create_pickup()
 func create_pickup() -> void:
 	var loot_instance = LOOT_SCENES[randi() % LOOT_SCENES.size()].instantiate()
 	_path_follow.add_child.call_deferred(loot_instance)
