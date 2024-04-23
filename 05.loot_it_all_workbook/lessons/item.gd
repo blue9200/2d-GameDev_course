9b567f2143a47674b3d421958e9db8459d64730f
blue9200 <Josantoyo@s.sfusd.edu>
2024-04-23 10:36:25 -0700

    M5 L.10

--- 05.loot_it_all_workbook/lessons/item.gd
+++ 05.loot_it_all_workbook/lessons/item.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	play_floating_animation()
 
+func play_floating_animation() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	
+	var sprite_2d := get_node("Sprite2D")
+	var position_offset := Vector2(0.0, 4.0)
+	var duradtion = randf_range(0.8, 1.2)
+	var duration := 1.0
+	tween.tween_property(sprite_2d, "position", position_offset, duration)
+	tween.tween_property(sprite_2d, "position", -1.0 * position_offset, duration)
+	tween.set_loops()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
